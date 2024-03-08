5614ef6e415d95edd0cc9a66cfd8aebc46368747
AndresBLV <77251658+AndresBLV@users.noreply.github.com>
2024-03-08 10:23:09 -0400

    Firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjQhZ8Rxk-85ff5t4EQPH2YHdobjsuHXg",
+  authDomain: "microproyecto-2-c7a0e.firebaseapp.com",
+  projectId: "microproyecto-2-c7a0e",
+  storageBucket: "microproyecto-2-c7a0e.appspot.com",
+  messagingSenderId: "638432167898",
+  appId: "1:638432167898:web:4c51ae1f4f7851f5f6a2e3",
+  measurementId: "G-N863H4F7J4"
+};
+
+const app = initializeApp(firebaseConfig);
