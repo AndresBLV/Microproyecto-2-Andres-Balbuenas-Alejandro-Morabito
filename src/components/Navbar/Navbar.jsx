97aa131523c95a713b3477e47bee79f8bf01a4ec
AndresBLV <77251658+AndresBLV@users.noreply.github.com>
2024-03-09 01:04:52 -0400

    Navbar Listo, Inicio HOMEPAGE

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,42 @@
 import {Link} from "react-router-dom";
 import { CLUB_URL, HOME_URL, LOGIN_URL, REGISTER_URL, VIDEOGAMES_URL } from "../../constanst/urls";
-import "./Navbar.css";
+import styles from "./Navbar.module.css";
 
 export function Navbar(){
-    return(
-    <>
-    <nav className="NavContainer">
-        <div>
-            <Link to={HOME_URL} className="styleLink">
-                <span>Inicio</span>
-            </Link>
-            <Link to={CLUB_URL} className="styleLink">
-                <span>Clubs</span>
-            </Link>
-            <Link to={VIDEOGAMES_URL} className="styleLink">
-                <span>Videojuegos</span>
-            </Link>
-            <Link to={LOGIN_URL} className="styleLink">
-                <span>Iniciar Sesion</span>
-            </Link>
-            <Link to={REGISTER_URL} className="styleLink">
-                <span>Registrarse</span>
-            </Link>
-        </div>
-    </nav>
-    </>
-    )
+    return (
+        <nav className={styles.navbar}>
+            <ul className={styles.menuList}>
+                <li className={`${styles.menuItem} ${styles.menuItemLeft}`}>
+                    <Link to={HOME_URL} className={styles.link}>
+                        <span>Inicio</span>
+                    </Link>
+                </li>
+                <li className={`${styles.menuItem} ${styles.menuItemLeft}`}>
+                    <Link to={CLUB_URL} className={styles.link}>
+                        <span>Clubes</span>
+                    </Link>
+                </li>
+                <li className={`${styles.menuItem} ${styles.menuItemLeft}`}>
+                    <Link to={VIDEOGAMES_URL} className={styles.link}>
+                        <span>Videogames</span>
+                    </Link>
+                </li>
+            </ul>
+    
+            <ul className={styles.menuList}>
+                <>
+                    <li className={`${styles.menuItem} ${styles.menuItemRight}`}>
+                        <Link to={LOGIN_URL} className={styles.link}>
+                            <span>Iniciar sesión</span>
+                        </Link>
+                    </li>
+                    <li className={`${styles.menuItem} ${styles.menuItemRight}`}>
+                        <Link to={REGISTER_URL} className={styles.link}>
+                            <span>Registro</span>
+                        </Link>
+                    </li>
+                </>    
+            </ul>
+        </nav>
+    );
 }
