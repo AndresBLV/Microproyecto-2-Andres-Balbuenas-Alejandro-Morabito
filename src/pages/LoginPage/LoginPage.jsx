0e8031188d29ff88fb976ada64ae0745a82e66bf
AlejandroMorabito <amorabito@correo.unimet.edu.ve>
2024-03-09 00:38:04 -0400

    Iniciar sesión y Registro
    Base de iniciar sesión y de registro

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,12 @@
-
 export function LoginPage(){
     return(
-    <div>LoginPage</div>
+    <div class="Iniciar-Sesion">Iniciar Sesión
+        <br></br>
+        <input type="text" id="CorreoInput" placeholder="Correo"></input>
+        <br></br>
+        <input type="text" id="ContraseñaInput" placeholder="Contraseña"></input>
+        <br></br>
+        <button id="btnDraw" onclick=""> Iniciar Sesión</button>
+    </div>
     )
 }
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,6 +1,20 @@
 
 export function RegisterPage(){
     return(
-    <div>RegisterPage</div>
+    <div class="Registrarse">Registrarse
+        <br></br>
+        <input type="text" id="NombreInput" placeholder="Nombre"></input>
+        <input type="text" id="ApellidoInput" placeholder="Apellido"></input>
+        <br></br>
+        <input type="text" id="CorreoInput" placeholder="Correo"></input>
+        <br></br>
+        <input type="text" id="ContraseñaInput" placeholder="Contraseña"></input>
+        <br></br>
+        <button id="btnRegistrarse" onclick=""> Registrarse</button>
+        <br></br>
+        <br></br>
+        <button id="btnGoogle" onclick=""> Registrarse con google</button>
+        <button id="btnX" onclick=""> Registrarse con X</button>
+    </div>
     )
 }
